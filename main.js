c7900933ef32ba2f834918323dcec17c1ce4742c
maria carmona <majotorco14@gmail.com>
2023-02-18 19:41:55 -0500

    segunda entrega

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 
 function pedirDato (){
-    let dato = prompt("Ingrese su nombre y apellido")
+    let dato = prompt("Ingresa tu apodo estrella")
     let mensaje = `Hola ${dato} Bienvenido a Mi Presupuesto`
     alert(mensaje)
 }
 pedirDato()
 
-let presupuesto = prompt("ingrese el destino del presupuesto, ejemplo: Hogar,Vacaciones o automovil")
-if (presupuesto == "Hogar".toUpperCase){
-    alert("Presupuesto Hogar.");
-}else if (presupuesto == "Vacaciones".toUpperCase){
-    alert("Presupuesto Vacaciones.");
-}else if (presupuesto == "Automovil".toUpperCase){
-    alert("Presupuesto para Automivil.");
-}
+const usuarios = {
+    nombre: prompt("Ingrese Nombre y Apellido"),
+    edad: prompt("Cuantos años tienes?"),
+    pais: prompt("Ingrese su pais de residencia"),
+    salario: parseInt(prompt("Ingrese el valor de su salario")),
+};
+
+console.log(usuarios.nombre);
+console.log(usuarios.edad);
+console.log(usuarios.pais);
+console.log(usuarios.salario);
 
 let monedaIngresos = prompt("en que moneda recibe sus ingresos.");
 
@@ -35,14 +38,51 @@ switch (monedaIngresos) {
         break;
 }
 
+let presupuesto = prompt("ingrese el destino del presupuesto, ejemplo: Hogar,Vacaciones o automovil")
+if (presupuesto.toUpperCase() == "HOGAR"){
+    alert("Presupuesto Hogar.");
+}else if (presupuesto.toUpperCase() == "VACACIONES"){
+    alert("Presupuesto Vacaciones.");
+}else if (presupuesto.toUpperCase() == "AUTOMIVIL"){
+    alert("Presupuesto para Automivil.");
+}
 
+let precioLuz = parseInt(prompt("ingrese el valor de su servicio de luz"))
+let precioAgua = parseInt(prompt("ingrese el valor de su servicio de agua"))
+let precioGas = parseInt(prompt("ingrese el valor de su servicio de gas"))
+let resultado = 0
 
+const presupuestoHogar = [
+    { id: 1, servicio: "luz", precio: precioLuz },
+    {id: 2, servicio: "agua", precio: precioAgua},
+    {id: 3, servicio: "gas", precio: precioGas },
+];
 
+for (let index = 0; index < presupuestoHogar.length; index++){
+    const element = presupuestoHogar[index];
+    resultado += element.precio
+}
 
+alert(resultado);
 
-
-
-
-
-
-
+alert("Presupuesto Vacaciones")
+const presupuestoVacaciones = [
+        { id: 1, nombre: "vuelo", precio: 800000},
+        { id: 2, nombre: "hotel", precio: 250000 },
+        { id: 3, nombre: "tours", precio: 600000 },
+        { id: 4, nombre: "transportes", precio: 180000 },
+    ];
+    
+    let precio = parseInt(prompt("Ingrese el valor mínimo a gastar"));
+    let filtrados = presupuestoVacaciones.filter((item) => item.precio > precio);
+    let mensaje = "";
+    
+    filtrados.forEach((item) => {
+        mensaje += `
+        ID: ${item.id}
+        Nombre: ${item.nombre}
+        Precio: $${item.precio}
+        `;
+    });
+    
+    alert(mensaje);
